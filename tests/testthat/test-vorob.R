9d1a121fd9554d869dd53c9e6a965a54e7bf854d
mbinois <mbinois@mcs.anl.gov>
2018-05-30 15:59:19 -0500

    Add tests for vorob functions

---
+++ tests/testthat/test-vorob.R
@@ -0,0 +1,17 @@
+library(eaf)
+context("vorob")
+
+test_that("vorob", {
+  #skip_on_cran()
+  test_data <- expand.grid(seq(0, 1, length.out = 51),
+                           seq(0, 1, length.out = 51))
+  test_data <- as.matrix(cbind(test_data, nrun = 1:nrow(test_data)))
+  
+  # Average hypervolume is known to be 0.25
+  # avg_hyp <- mean(sapply(split.data.frame(test_data[,1:2], test_data[,3]),
+  #                        hypervolume, reference = c(1, 1))) 
+  expect_equal(hypervolume(vorobT(x = test_data, reference = c(1, 1))$VE, reference = c(1, 1)), 0.25, tolerance = 1e-1)
+  
+  expect_equal(vorobDev(x = test_data, VE = vorobT(test_data, c(1,1))$VE, reference = c(1, 1)), 0.218, tolerance = 1e-1)
+
+})
