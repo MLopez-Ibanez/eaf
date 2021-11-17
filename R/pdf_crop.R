a60a887bffa88cd798f177af2b44448cfef10c49
MLopez-Ibanez <2620021+MLopez-Ibanez@users.noreply.github.com>
2021-11-17 17:07:38 +0100

    New function pdf_crop.

---
+++ R/pdf_crop.R
@@ -0,0 +1,34 @@
+#' Remove whitespace margins using https://ctan.org/pkg/pdfcrop
+#'
+#' @param filename Filename of a PDF file to crop. The file will be overwritten.
+#' @param mustWork If `TRUE`, then give an error if the file cannot be cropped.
+#' @param pdfcrop Path to the `pdfcrop` utility.
+#' @return Nothing
+#' 
+#' @examples
+#' \dontrun{
+#' extdata_path <- system.file(package = "eaf", "extdata")
+#' A1 <- read_datasets(file.path(extdata_path, "ALG_1_dat.xz"))
+#' A2 <- read_datasets(file.path(extdata_path, "ALG_2_dat.xz"))
+#' pdf(file = "eaf.pdf", onefile = TRUE, width = 5, height = 4)
+#' eafplot(list(A1 = A1, A2 = A2), percentiles = 50, sci.notation=TRUE)
+#' dev.off()
+#' pdf_crop("eaf.pdf", mustWork=TRUE)
+#' }
+#' @export
+#' @md
+pdf_crop <- function(filename, mustWork = FALSE, pdfcrop = Sys.which("pdfcrop"))
+{
+  if (is.null(pdfcrop) || pdfcrop == "") {
+      if (mustWork) {
+        stop("pdfcrop not found!")
+      } else {
+        warning("pdfcrop not found, not cropping")
+      }
+  } else if (!file.exists(filename)) {
+      stop("PDF file", shQuote(filename), "not found")
+  } else {
+    try(system2(pdfcrop, c("--pdfversion 1.5", filename, filename)))
+  }
+}
+
