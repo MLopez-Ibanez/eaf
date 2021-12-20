12a757e2264de1980ad532ff2537039214817445
MLopez-Ibanez <2620021+MLopez-Ibanez@users.noreply.github.com>
2021-12-20 11:01:40 +0100

    update README figure.

--- tools/create-figures.R
+++ tools/create-figures.R
@@ -10,7 +10,7 @@ invisible(dev.off())
 a1 <- read_datasets(file.path(extdata_dir, "wrots_l100w10_dat"))
 a2 <- read_datasets(file.path(extdata_dir, "wrots_l10w100_dat"))
 # FIXME: How to crop PNG?
-png(filename = "man/figures/eafdiff-color.png", width = 960, height = 480)
+png(filename = "man/figures/eafdiff-color.png", width = 600, height = 350)
 eafdiffplot(a1, a2, col = colorRampPalette(c("blue", "red")), intervals = 10,
             title.left = expression("W-RoTS, " * lambda * "=" * 100 * ", " * omega * "=" * 10),
             title.right= expression("W-RoTS, " * lambda * "=" * 10 * ", " * omega * "=" * 100))
