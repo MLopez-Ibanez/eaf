405b4e26ef72f3b023bce84e295f94edf0f26f77
MLopez-Ibanez <2620021+MLopez-Ibanez@users.noreply.github.com>
2023-03-20 02:53:04 +0000

    Move tests/lin-sph.R to tests/testthat/test-eaf.R

--- tests/testthat/test-eaf.R
+++ tests/testthat/test-eaf.R
@@ -1,7 +1,7 @@
 source("helper-common.R")
 
 test_that("eaf", {
-  #skip_on_cran()
+
   test.eaf.dataset <- function(name, percentiles = NULL) {
     dataset <- get(name)
     x <- eaf:::compute.eaf(dataset, percentiles)
@@ -21,11 +21,27 @@ test_that("eaf", {
   expect_equal(test.eaf.dataset("SPEA2relativeRichmond"),
                readRDS("SPEA2relativeRichmond-eaf.rds"))
 
-  for(i in seq_len(399))
+  for (i in seq_len(399))
     expect_equal(anyDuplicated(eafs(cbind(0:i, 0:i), 0:i)[,1]), 0L)
 })
 
 test_that("eafs_sets_numeric", {
   expect_error(eafs(matrix(1:10, ncol=2), sets=letters[1:5]),
                "sets")
+})
+
+test_that("eaf3d", {
+  lin <- read_datasets("lin.S.txt")
+  sph <- read_datasets("sph.S.txt")
+  nobjs <- ncol(lin) - 1
+  nruns.left <- max(lin[, nobjs + 1])
+  data.combined <- sph
+  data.combined[, nobjs + 1] <- data.combined[, nobjs + 1] + nruns.left
+  data.combined <- rbind(lin, data.combined)
+  # This may stop working once we filter uninteresting values in the C code directly.
+  DIFF <- eaf:::compute.eafdiff.helper(data.combined, intervals = nruns.left)
+  x <- as.matrix(read.table("lin.S-sph.S-diff.txt.xz", header = FALSE))
+  dimnames(x) <- NULL
+  x[, nobjs + 1] <- x[, nobjs + 1] - x[, nobjs + 2]
+  expect_equal(DIFF[, 1 : (nobjs + 1)], x[, 1 : (nobjs + 1)])
 })
